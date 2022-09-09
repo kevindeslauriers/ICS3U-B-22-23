f6e77734724078ca5c3b9248bcf9cbabb26d1a63
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-09-09 11:07:22 -0400

    Initial Commit

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,28 @@
+package day2;
+
+/*
+ * This is a multi line comment
+ * Comments are ignored by the compiler (Java) and are meant for developers to understand what the code is meant to do.
+ */
+public class ExampleOne {
+   public static void main(String[] args) {
+      System.out.println("Welcome to Computer Science");
+      System.out.println("Welcome to Computer Science");
+      System.out.println("Welcome to Computer Science");
+
+      // System.out.println(); // this line is ignored because it is a comment
+
+      System.out.print("A");
+      System.out.print("B");
+      System.out.print("C");
+      System.out.println("D");
+      System.out.print("E");
+      System.out.println("F");
+
+      // ABCD
+      // EF
+      //
+
+   }
+
+}
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,17 @@
+package day2;
+
+/*
+ * This example is about escape sequences
+ */
+public class ExampleTwo {
+   public static void main(String[] args) {
+      System.out.println("Computer Scie\nce is the \"BEST\" class ever!");
+      System.out.println("/\\\"");
+
+      // escape sequence is meant to give another meaning to a character
+      // AP Exam cares abot \\ \" and \n
+      // \n is new line
+      // \" displays a quote
+      // \\ displays a backslash
+   }
+}
