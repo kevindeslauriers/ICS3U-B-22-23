d3cb5c912c5f12a0acac0bf86fff948707b89e52
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-09-21 10:33:14 -0400

    Sample Question

---
+++ Unit One - Introduction to Computer Science/src/day4/PracticeOne.java
@@ -0,0 +1,19 @@
+package day4;
+
+/**
+ * PracticeOne
+ */
+public class PracticeOne {
+
+   public static void main(String[] args) {
+      /* Calculate and displace the circumference of a circle */
+
+      double radius = 4;
+      final double PI = 3.14;
+
+      double circumference = 2 * PI * radius;
+
+      System.out.println("The Circumference of a circle with radius " + radius + " is: " + circumference);
+
+   }
+}
