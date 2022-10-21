17bf03f7c9e4a02ac2ca515a2ba7c1f09b8f359d
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-10-21 08:40:07 -0400

    Update Booleans

--- Unit Three - Booleans and Selection/src/day1/BooleanVariables.java
+++ Unit Three - Booleans and Selection/src/day1/BooleanVariables.java
@@ -2,6 +2,17 @@ package day1;
 
 public class BooleanVariables {
    public static void main(String[] args) {
+      // declare a boolean variable to represent if a door is locked
+
+      // declare a boolean variable to represent if a number is odd
+
+      // declare a boolean variable to represent whether it is raining
+
+      // declare a boolean variable to represent if a player has a shield
+
+      // assign values to the variables you declared above
+
+      // print the values to the console with the variable name in front of it
 
    }
 }
---
+++ Unit Three - Booleans and Selection/src/day1/CodingBatLogic1.java
@@ -0,0 +1,5 @@
+package day1;
+
+public class CodingBatLogic1 {
+   
+}
