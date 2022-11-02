390be6ca083170feca700ac203d574da1b068392
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-11-02 09:32:59 -0400

    Loops

---
+++ Unit Four - Repetition with Loops/src/day1/WhileLoopExamples.java
@@ -0,0 +1,36 @@
+package day1;
+
+public class WhileLoopExamples {
+   public static void main(String[] args) {
+      exampleOne();
+      exampleTwo();
+      exampleThree();
+      exampleFour();
+   }
+
+   /*
+    * Introduction to While Loops - Keep asking for a number until the number is
+    * even.
+    */
+   private static void exampleOne() {
+
+   }
+
+   /* Sum the numbers from 1 to 1000 using a while loop */
+   private static void exampleTwo() {
+
+   }
+
+   /*
+    * Prompt the user for a number and add it to a sum. Continue as long as the
+    * number is positive
+    */
+   private static void exampleThree() {
+
+   }
+
+   /* Prompt the user for a String and count the number of vowels in the String. */
+   private static void exampleFour() {
+   }
+
+}
