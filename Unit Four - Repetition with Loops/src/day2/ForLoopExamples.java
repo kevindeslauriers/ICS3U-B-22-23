495b4063c21d8c21c755c416dcea1fca9667272f
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-11-04 09:22:09 -0400

    For Loops

---
+++ Unit Four - Repetition with Loops/src/day2/ForLoopExamples.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class ForLoopExamples {
+   public static void main(String[] args) {
+      exampleOne();
+      exampleTwo(1, 1000);
+      exampleThree("Check this String");
+
+   }
+
+   /* Introduction to for loops (counting loops) */
+   private static void exampleOne() {
+
+   }
+
+   /* Obtain the sum of the numbers from start to end */
+   private static void exampleTwo(int start, int end) {
+
+   }
+
+   /* count the number of words in the sentence */
+   private static void exampleThree(String sentence) {
+
+   }
+}
