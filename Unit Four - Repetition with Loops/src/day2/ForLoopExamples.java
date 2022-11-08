ad5294e568d40d59aebec01a4c823cb2fd5aa21e
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2022-11-08 09:19:16 -0500

    Comparing Objects

--- Unit Four - Repetition with Loops/src/day2/ForLoopExamples.java
+++ Unit Four - Repetition with Loops/src/day2/ForLoopExamples.java
@@ -2,24 +2,67 @@ package day2;
 
 public class ForLoopExamples {
    public static void main(String[] args) {
-      exampleOne();
-      exampleTwo(1, 1000);
+      // exampleOne();
+      // exampleTwo(1, 3);
       exampleThree("Check this String");
 
    }
 
    /* Introduction to for loops (counting loops) */
    private static void exampleOne() {
+      // int i = 0;
+      // int sum = 0;
+      // while (i < 10) {
+      // sum += i;
+      // i++;
+      // }
+      int sum = 0;
+      // for loop for(init counter; condition; increment counter) {}
+      for (int i = 0; i < 10; i++) {
+         sum += i;
+      }
+
+      // initialise (once)
+      // check the condition
+      // body
+      // incrememt
+      // condition
+      // body
+      // incrememt
+      // condition
+      // body
+      // incrememt
+      // condition
+      // body
+
+      // does not initialise
+      // does not incrememnt
+      // runs forever -> infinite loop
+      // for (; true;) {
+
+      // }
 
    }
 
    /* Obtain the sum of the numbers from start to end */
    private static void exampleTwo(int start, int end) {
+      int sum = 0;
 
+      for (int i = start; i <= end; i++) {
+         sum += i;
+      }
+
+      System.out.println(sum);
    }
 
    /* count the number of words in the sentence */
    private static void exampleThree(String sentence) {
+      int numWords = 1;
+      for (int i = 0; i < sentence.length(); i++) {
+         if (" ".equals(sentence.substring(i, i + 1)))
+            numWords++;
+      }
 
+      System.out.println(numWords + " words.");
    }
 }
---
+++ Unit Four - Repetition with Loops/src/day3/ComparingObjects.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class ComparingObjects {
+   public static void main(String[] args) {
+      exampleOne();
+      exampleTwo();
+      exampleThree();
+
+   }
+
+   private static void exampleThree() {
+   }
+
+   private static void exampleTwo() {
+   }
+
+   private static void exampleOne() {
+   }
+}
